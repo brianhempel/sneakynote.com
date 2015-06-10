46d2f0642dc8c6bcc7148878da64efe9fa21755c
Brian Hempel <bnhempel@mtu.edu>
2015-06-10 09:06:55 +0100

    start.sh updates

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 echo 3 | sudo tee /proc/sys/vm/drop_caches
 
-sudo \
-SNEAKYNOTE_PORT=443 \
+sudo sh -c "SNEAKYNOTE_PORT=443 \
 SNEAKYNOTE_CERTS=/home/sneakynote/src/github.com/brianhempel/sneakynote.com/sneakynote.com.certs \
 SNEAKYNOTE_PRIVATE_KEY=/home/sneakynote/src/github.com/brianhempel/sneakynote.com/sneakynote.com.key \
-/home/sneakynote/src/github.com/brianhempel/sneakynote.com/sneakynote.com
+/home/sneakynote/src/github.com/brianhempel/sneakynote.com/sneakynote.com >> /home/sneakynote/src/github.com/brianhempel/sneakynote.com/public/log.txt 2>&1 &"
