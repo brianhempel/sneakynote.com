162a66383ca1ad5116e6d05f1b5d40cefa78a6f3
Brian Hempel <bnhempel@mtu.edu>
2015-06-20 02:14:22 -0400

    Teardown script

---
+++ teardown.sh
@@ -0,0 +1,3 @@
+./stop.sh 2> /dev/null
+
+sudo sh -c "/home/sneakynote/src/github.com/brianhempel/sneakynote.com/sneakynote.com teardown >> /home/sneakynote/src/github.com/brianhempel/sneakynote.com/public/log.txt 2>&1 &" && echo "sneakynote.com datastore torn down"
