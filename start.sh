ff6875d3fd025e3e2b1d6dbfc267cc53ef476951
Brian Hempel <bnhempel@mtu.edu>
2015-06-20 00:49:41 -0400

    Success message on start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 sudo sh -c "SNEAKYNOTE_PORT=443 \
 SNEAKYNOTE_CERTS=/home/sneakynote/src/github.com/brianhempel/sneakynote.com/sneakynote.com.certs \
 SNEAKYNOTE_PRIVATE_KEY=/home/sneakynote/src/github.com/brianhempel/sneakynote.com/sneakynote.com.key \
-/home/sneakynote/src/github.com/brianhempel/sneakynote.com/sneakynote.com >> /home/sneakynote/src/github.com/brianhempel/sneakynote.com/public/log.txt 2>&1 &"
+/home/sneakynote/src/github.com/brianhempel/sneakynote.com/sneakynote.com >> /home/sneakynote/src/github.com/brianhempel/sneakynote.com/public/log.txt 2>&1 &" && echo "sneakynote.com started"
 
 sudo ./free_memory_maximizer.sh &
 echo $! > free_memory_maximizer.sh.pid
