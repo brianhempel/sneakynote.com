e08e0d320fa967045ffdb8721d07b3d113b4c41a
Brian Hempel <bnhempel@mtu.edu>
2017-06-10 13:48:21 -0500

    Direct periodic task output to logs as well.

--- start.sh
+++ start.sh
@@ -7,8 +7,8 @@ SNEAKYNOTE_CERTS=/home/sneakynote/src/github.com/brianhempel/sneakynote.com/lets
 SNEAKYNOTE_PRIVATE_KEY=/home/sneakynote/src/github.com/brianhempel/sneakynote.com/letsencrypt/config/live/sneakynote.com/privkey.pem \
 /home/sneakynote/src/github.com/brianhempel/sneakynote.com/sneakynote.com >> /home/sneakynote/src/github.com/brianhempel/sneakynote.com/log.txt 2>&1 &" && echo "sneakynote.com started"
 
-sudo ./periodically_renew_certs.sh &
+sudo ./periodically_renew_certs.sh >> /home/sneakynote/src/github.com/brianhempel/sneakynote.com/log.txt 2>&1 &
 echo $! > periodically_renew_certs.sh.pid
 
-sudo ./free_memory_maximizer.sh &
+sudo ./free_memory_maximizer.sh >> /home/sneakynote/src/github.com/brianhempel/sneakynote.com/log.txt 2>&1 &
 echo $! > free_memory_maximizer.sh.pid
